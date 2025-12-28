1423f8c9e4c0691c1ec8beed50836d50beb6f200
Abdulaziz-Albeshri <151993937+Abdulaziz-Albeshri@users.noreply.github.com>
2025-12-28 05:57:01 +0300

    Update package startup message for clarity

--- R/Rzzz.R
+++ R/Rzzz.R
@@ -2,5 +2,6 @@
 
 # This file is intentionally minimal - package hooks or onLoad can go here.
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("RFGeneRank: CV-stable predictive ranking for transcriptomics")
+  packageStartupMessage("RFGeneRank: Cross-validated, stable predictive gene ranking for transcriptomics")
 }
+
